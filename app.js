5211e069d5ce60a9e3efa830e8813c8a30e83ae5
jelly-as-bean <jelly@bean.com>
2022-01-22 15:57:40 +1300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello jelly-as-bean!'));
+app.get('/', (req, res) => res.send('Just when you thought you were safe!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
